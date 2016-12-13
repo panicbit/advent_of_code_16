cf238b0d7977e3264af860d8e8ef5ba601e95b23
panicbit <panicbit.dev@gmail.com>
2016-12-13 17:07:47 +0100

    Day 6.2

---
+++ day6_2/src/main.rs
@@ -0,0 +1,25 @@
+extern crate itertools;
+use itertools::Itertools;
+
+fn main() {
+    let input = include_str!("../input.txt");
+    let lines = input.lines().collect::<Vec<_>>();
+    let n_cols = lines.first().unwrap().chars().count();
+    
+    for col in 0..n_cols {
+        let mut chars = lines.iter()
+            .map(|line| line.chars().nth(col).unwrap())
+            .collect::<Vec<_>>();
+            
+        chars.sort();
+
+        let most_frequent_char = chars.iter()
+            .group_by(|c| *c).into_iter()
+            .map(|(key, elems)| (key, elems.count()))
+            .min_by_key(|kv| kv.1)
+            .unwrap().0;
+        print!("{}", most_frequent_char);
+    }
+    
+    println!("");
+}
