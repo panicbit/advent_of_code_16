12f30cc1ac5c3a0266414ebac0d4d20a2345cf66
panicbit <panicbit.dev@gmail.com>
2016-12-18 06:03:55 +0100

    Cleanup day 16

--- day16/src/dragon.rs
+++ day16/src/dragon.rs
@@ -1,5 +1,3 @@
-use std::ops::Index;
-
 use std::iter::{DoubleEndedIterator, once};
 use std::str::Chars;
 use std::iter::{Map, Rev, Once, Chain};
@@ -26,7 +24,7 @@ impl Dragon {
     pub fn len(&self) -> u32 {
         let x = self.data.len() as u32;
         let n = self.iteration;
-        x * 2_u32.pow(n) + geo_series(0, n-1, 2)
+        x * 2_u32.pow(n) + geo_series(n-1, 2)
     }
 
     pub fn chars<'a>(&'a self) -> DragonIter<'a> {
@@ -42,7 +40,7 @@ impl Dragon {
 }
 
 #[derive(Clone)]
-enum DragonIter<'a> {
+pub enum DragonIter<'a> {
     Head(Chars<'a>),
     Tail(Map<Rev<Box<DragonIter<'a>>>, Conv>),
     Glue(Chain<Chain<Box<DragonIter<'a>>, Once<char>>, Box<DragonIter<'a>>>),
@@ -71,34 +69,8 @@ impl<'a> DoubleEndedIterator for DragonIter<'a> {
 
 type Conv = fn (char) -> char;
 
-fn geo_series(from: u32, to: u32, k: u32) -> u32 {
+fn geo_series(to: u32, k: u32) -> u32 {
     (k.pow(to+1) - 1) / (k - 1)
-}
-
-#[test]
-fn test_dragon_len() {
-    assert_eq!(Dragon::new("10000", 23).len(), 23);
-    assert_eq!(Dragon::new("10000", 47).len(), 47);
-
-    assert_eq!(Dragon::new("10000", 22).len(), 23);
-    assert_eq!(Dragon::new("10000", 46).len(), 47);
-
-    assert_eq!(Dragon::new("10000", 24).len(), 47);
-    assert_eq!(Dragon::new("10000", 48).len(), 95);
-}
-
-#[test]
-fn test_dragon() {
-    let d = |n| {
-        let dragon = Dragon::new("1000", n as u32);
-        dragon.chars().collect::<String>()
-    };
-    assert_eq!(d(0), "100001110");
-    assert_eq!(d(9), "100001110");
-    assert_eq!(d(10), "1000011100100011110" );
-    assert_eq!(d(19), "1000011100100011110" );
-    assert_eq!(d(20), "100001110010001111001000011101100011110");
-    assert_eq!(d(39), "100001110010001111001000011101100011110");
 }
 
 fn conv(ch: char) -> char {
