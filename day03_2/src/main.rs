f8df5b61f2b80ab7edefd28aa6b9081095003c93
panicbit <panicbit.dev@gmail.com>
2017-01-18 17:11:41 +0100

    Day 3.2: Refactor

--- day03_2/src/main.rs
+++ day03_2/src/main.rs
@@ -1,32 +1,28 @@
+extern crate itertools;
+extern crate multizip;
+use itertools::Itertools;
+use multizip::zip3;
 
 fn main() {
     let input = include_str!("../input.txt");
-    let mut col1 = Vec::new();
-    let mut col2 = Vec::new();
-    let mut col3 = Vec::new();
 
-    for line in input.lines() {
-        let mut values = line.split_whitespace().map(|n| n.parse::<u32>().unwrap());
-        col1.push(values.next().unwrap());
-        col2.push(values.next().unwrap());
-        col3.push(values.next().unwrap());
-    }
+    let n = input.lines()
+        .chunks(3).into_iter()
+        .flat_map(|mut lines|
+            zip3(
+                lines.next().unwrap().split_whitespace().map(|n| n.parse::<u32>().unwrap()),
+                lines.next().unwrap().split_whitespace().map(|n| n.parse::<u32>().unwrap()),
+                lines.next().unwrap().split_whitespace().map(|n| n.parse::<u32>().unwrap()),
+            )
+        )
+        .filter(is_triangle)
+        .count();
 
-    let n =   triangles_in_column(&col1)
-            + triangles_in_column(&col2)
-            + triangles_in_column(&col3);
-    println!("{}", n);
+    println!("{:?}", n);
 }
 
-fn triangles_in_column(col: &[u32]) -> usize {
-    col.chunks(3)
-    .filter(|triangle| is_triangle(&triangle))
-    .count()
-}
-
-fn is_triangle(t: &[u32]) -> bool {
-       t.len() == 3
-    && t[0] + t[1] > t[2]
-    && t[1] + t[2] > t[0]
-    && t[2] + t[0] > t[1]
+fn is_triangle(&(a, b, c): &(u32, u32, u32)) -> bool {
+       a + b > c
+    && b + c > a
+    && c + a > b
 }
